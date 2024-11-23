50fe058b1062a7f997ce43fc40bc02dcf1c6fa66
NassimaAgram <agram.nassima@gmail.com>
2024-11-23 19:11:27 +0100

    .gitignore

--- server/dist/src/middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requireAuthentication = void 0;
-const express_1 = require("@clerk/express");
-// Middleware for strict authentication
-exports.requireAuthentication = express_1.requireAuth;
