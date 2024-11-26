a9c37404907e6da88da57445089a8528fd5dd551
NassimaAgram <agram.nassima@gmail.com>
2024-11-26 10:27:25 +0100

    fixed redux store

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import React from "react";
-import { DashboardNavbar, DashboardSidebar} from "@/components";
+import { DashboardNavbar, DashboardSidebar } from "@/components";
 import StoreProvider from "@/app/redux";
 
 interface Props {
@@ -7,16 +9,13 @@ interface Props {
 }
 
 const DashboardLayout = ({ children }: Props) => {
-
   return (
     <StoreProvider>
       <div className="flex flex-col min-h-screen w-full">
         <DashboardNavbar />
         <main className="flex flex-col lg:flex-row flex-1 size-full">
           <DashboardSidebar />
-          <div className="w-full pt-14 lg:ml-72">
-            {children}
-          </div>
+          <div className="w-full pt-14 lg:ml-72">{children}</div>
         </main>
       </div>
     </StoreProvider>
